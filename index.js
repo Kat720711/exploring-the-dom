5267a6df5a17c18911cb34cf142eaba903a3ffdd
Kat720711 <ckmakofane2001@gmail.com>
2024-07-28 11:22:04 +0200

    gh-pages

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-const myMessage = document.querySelector('myMessage');
-const theMessageButton = document.querySelector('.theMesageButton');
-const clearMessageButton = document.querySelector('.clearMessageButton');
-const inputBox = document.querySelector('.inputBox');
-const fruits = ['Apples', 'Pears', 'Oranges', 'Grapes', 'Bananas'];
-const fruitlist = document.querySelector('.fruits');
+const myMessage = document.querySelector('.myMessage');
+const theMessageButton = document.querySelector('.theMessageButton');
+const inputBox = document.querySelector('.theInputValue');
+const fruitList = document.querySelector('.fruits');
+const addFruitButton = document.querySelector('.addFruitButton');
 
-theMessageButton.addEventListener('click', function(){
-    if (inputBox.ariaValueMax.trim().length > 0) {
-        myMessage.innerText = inputBox.ariaValueMax;
+setTimeout(function() {
+    myMessage.innerText = 'This is a message in the DOM!';
+}, 3000);
+
+theMessageButton.addEventListener('click', function() {
+    if (inputBox.value.trim().length > 0) {
+        myMessage.innerText = inputBox.value;
+    } else {
+        myMessage.innerText = 'Please enter a message';
     }
 });
 
-clearMessageButton.addEventListener('click', function(){
-    myMessage.innerText = ''
-})
+myMessage.addEventListener('click', function() {
+    myMessage.classList.toggle('darkmode');
+});
 
-myMessage.addEventListener('click', function(){
-    myMessage.classList.toggle('darkmode')
-})
+const fruits = ['Apples', 'Pears', 'Oranges', 'Grapes', 'Bananas'];
 
-for(let i=0; i<fruits.length; i++) {
+for (let i = 0; i < fruits.length; i++) {
     const fruit = fruits[i];
-
     const li = document.createElement('li');
     li.innerText = fruit;
-    fruitlist.appendChild(li);
+    fruitList.appendChild(li);
 }
+
+addFruitButton.addEventListener('click', function() {
+    const newFruit = prompt('Enter a new fruit:');
+    if (newFruit) {
+        const li = document.createElement('li');
+        li.innerText = newFruit;
+        fruitList.appendChild(li);
+    }
+});
+
